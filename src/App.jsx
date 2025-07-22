eff42de4fc4d0544369791971a5bf3a48703c652
Semen Prokopets <semen2004vuz@yandex.ru>
2025-07-22 18:41:50 +0400

    исправил адаптив под мобилку

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 
 export default function App() {
-    return (
-        <Router>
-            <AppRoutes />
-        </Router>
-    );
+    return <AppRoutes />;
 }
